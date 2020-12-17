d37b026f9b614a3f1403f1cc2f5b3a65cb6a3469
TrymTv <tiltrym@gmail.com>
2020-12-18 00:34:21 +0100

    Day 6

---
+++ 6/6.py
@@ -0,0 +1,21 @@
+inputData = []
+with open("6/input.txt", "r") as inputFile:
+    for line in inputFile:
+        inputData.append(line.strip())
+
+answerSet = set()
+groups = []
+
+for elem in inputData:
+    if elem != "":
+        for char in elem:
+            answerSet.add(char)
+    else:
+        groups.append(answerSet)
+        answerSet = set()
+
+sum = 0
+for answers in groups:
+    sum += len(answers)
+
+print(sum)
