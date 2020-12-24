3342b5755e8c0f7ffd0dacc42bd476f0fc07399c
TrymTv <tiltrym@gmail.com>
2020-12-24 21:48:19 +0100

    Day 8

--- 8/8.py
+++ 8/8.py
@@ -1,3 +1,5 @@
+import copy
+
 # Part 1
 inputData = []
 with open("8/input.txt", "r") as inputFile:
@@ -5,10 +7,10 @@ with open("8/input.txt", "r") as inputFile:
         line = line.strip().split()
         inputData.append(line + [False])
 
+inputCopy = copy.deepcopy(inputData)
 operations = {}
 accumilator = 0
 op = 0
-nextOp = inputData[0]
 
 
 def incOp(num):
@@ -31,8 +33,35 @@ operations["nop"] = incOp
 operations["acc"] = acc
 operations["jmp"] = jmp
 
-while not inputData[op][2]:
-    inputData[op][2] = True
-    operations[inputData[op][0]](inputData[op][1])
 
+def execute():
+    while not inputData[op][2]:
+        inputData[op][2] = True
+        operations[inputData[op][0]](inputData[op][1])
+        if op == len(inputData):
+            return True
+
+
+execute()
 print(accumilator)
+
+# Part 2
+
+
+def resetInput():
+    global inputData
+    inputData = copy.deepcopy(inputCopy)
+
+
+resetInput()
+nopJmpPair = ["nop", "jmp"]
+for index, operation in enumerate(inputData):
+    op = 0
+    accumilator = 0
+    if operation[0] in nopJmpPair:
+        inputData[index][0] = "jmp" if operation[0] == "nop" else "nop"
+        if execute():
+            print(accumilator)
+            break
+        else:
+            resetInput()
