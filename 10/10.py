b075a5e94cca0d0d9e4467c5ba354f466830b54f
TrymTv <tiltrym@gmail.com>
2020-12-27 20:37:45 +0100

    Day 10 part 1 and 2

---
+++ 10/10.py
@@ -0,0 +1,39 @@
+# Part 1
+
+inputData = []
+with open("10/input.txt", "r") as inputFile:
+    for line in inputFile:
+        inputData.append(int(line.strip()))
+inputData.append(0)
+
+sortedList = sorted(inputData)
+
+sortedList.append(sortedList[-1] + 3)
+
+diffSums = [0, 0, 0, 0]
+
+for index, elem in enumerate(sortedList):
+    if index != len(sortedList)-1:
+        diff = sortedList[index + 1] - elem
+        diffSums[diff] += 1
+    else:
+        break
+
+print(diffSums[1] * diffSums[3])
+
+
+# Part 2
+
+numOfChoises = [1]
+
+reversedList = list(reversed(sorted(inputData)))
+
+for i, e in enumerate(reversedList[1:]):
+    numOfChoises.append(0)
+    for subI, subE in reversed(list(enumerate(reversedList[:i+1]))):
+        if subE - e <= 3:
+            numOfChoises[-1] += numOfChoises[subI]
+        else:
+            break
+
+print(numOfChoises[-1])
